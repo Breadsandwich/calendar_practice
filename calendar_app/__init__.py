8e4631a3cba6fdf322b05f6b2dba707009c4b34e
jstnswn <jstnswn@gmail.com>
2022-02-22 15:45:24 -0800

    Create first route to diplay appointment name and times

--- calendar_app/__init__.py
+++ calendar_app/__init__.py
@@ -1,9 +1,10 @@
-from calendar_app.routes.cal_routes import bp
-from flask import Flask
+from flask import Flask, render_template
 import os
+
+from calendar_app.routes.cal_routes import bp
+
 
 
 app = Flask(__name__)
 app.config.update({'SECRET_KEY': os.environ.get('SECRET_KEY')})
 app.register_blueprint(bp)
-
--- calendar_app/routes/cal_routes.py
+++ calendar_app/routes/cal_routes.py
@@ -1,7 +1,35 @@
 from flask import Blueprint, render_template
+import os
+import psycopg2
 
 bp = Blueprint('calendar', __name__, url_prefix='/calendar')
 
-@bp.route("/")
-def test():
-    return render_template('main.html')
+# @bp.route("/")
+# def test():
+#     return render_template('main.html')
+
+CONNECTION_PARAMETERS = {
+    "user": os.environ.get("DB_USER"),
+    "password": os.environ.get("DB_PASS"),
+    "dbname": os.environ.get("DB_NAME"),
+    "host": os.environ.get("DB_HOST"),
+}
+
+@bp.route('/')
+def main():
+    print('hello??')
+    with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
+        with conn.cursor() as curs:
+            # def get_all_appointments():
+            curs.execute(
+                """
+                SELECT id, name, start_datetime, end_datetime
+                FROM appointments
+                ORDER BY start_datetime;
+                """
+            )
+            results = curs.fetchall()
+        # results = get_all_appointments()
+        print('!!!!!!!!!', results)
+
+    return render_template('main.html', rows=results)
