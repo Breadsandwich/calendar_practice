e7e227a002d3cad9b834e7350f105533bfa8c533
daniel <danielthai14@gmail.com>
2022-02-22 17:27:37 -0700

    new appointment route set up

---
+++ calendar_app/forms.py
@@ -0,0 +1,15 @@
+
+from xmlrpc.client import DateTime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, SubmitField, DateField, TimeField, TextAreaField, BooleanField
+from wtforms.validators import DataRequired
+
+class NewAppointment(FlaskForm):
+    name = StringField('Name')
+    start_date = DateField('Start Date')
+    start_time = TimeField('Start Time')
+    end_date = DateField('End Date')
+    end_time = TimeField('End Time')
+    description = TextAreaField('Description')
+    private = BooleanField('Private')
+    submit = SubmitField('Submit Appointment')
--- calendar_app/routes/cal_routes.py
+++ calendar_app/routes/cal_routes.py
@@ -1,8 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template
 import os
 import psycopg2
+from calendar_app.forms import NewAppointment
 
 bp = Blueprint('calendar', __name__, url_prefix='/calendar')
+
+
 
 # @bp.route("/")
 # def test():
@@ -33,3 +36,45 @@ def main():
         print('!!!!!!!!!', results)
 
     return render_template('main.html', rows=results)
+
+
+@bp.route('/new_appointment', methods=['GET','POST'])
+def new_appoint():
+    form = NewAppointment()
+
+    if form.validate_on_submit():
+        name = form.data['name']
+        start_date = form.data['start date']
+        start_time = form.data['start time']
+        end_date = form.data['end date']
+        end_time = form.data['end time']
+        description = form.data['description']
+        private = form.data['private']
+
+        with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
+            with conn.cursor() as curs:
+                curs.execute(
+                    '''
+                    INSERT INTO appointments (name, start_date, start_time, end_date, end_time, description, private)
+                    VALUES (
+                        %(name)s,
+                        %(start_date)s,
+                        %(start_time)s,
+                        %(end_date)s,
+                        %(end_time)s,
+                        %(description)s,
+                        %(private)s
+                    )
+                    ''',
+                    {
+                        "name": name,
+                        "start_date": start_date,
+                        "start_time": start_time,
+                        "end_date": end_date,
+                        "end_time": end_time,
+                        "description": description,
+                        "private": private,
+                    }
+                )
+        return redirect('/calendar')
+    return render_template('main.html', form=form)
